69971230e9c13a8c30b114e300e4a1182c514a2d
abner.chamorro <abner.chamorro@peiky.com>
2020-12-13 16:03:32 -0500

    make it better

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <ListProduct />
+  </div>
+</template>
+
+<script>
+import ListProduct from "./components/ListProduct.vue";
+
+export default {
+  name: "App",
+  components: {
+    ListProduct
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: orange;
+  color: white;
+  height: 100vh;
+}
+p {
+  margin-bottom: 0;
+  margin-top: 0;
+}
+</style>
---
+++ src/components/ListProduct.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="list">
+    <header>
+      <h2>
+        Bienvenido a Quiles
+      </h2>
+    </header>
+    <section v-for="(product, idx) in products" :key="idx">
+      <img :src="product.foto_producto" :alt="product.nombre" />
+      <div class="text-item">
+        <div class="title">
+          <h3>{{ product.nombre }}</h3>
+          <span>
+            {{ product.precio }}
+          </span>
+        </div>
+        <p>
+          {{ product.descripcion }}
+        </p>
+      </div>
+      <div class="line-horizontal"></div>
+    </section>
+
+    <!-- <section>
+      <img
+        src="http://localhost:8000/media/burguer_ltZlWTt.jpg"
+        alt="Hamburguesa doble carne"
+      />
+      <div class="text-item">
+        <div class="title">
+          <h3>Hamburguesa doble carne</h3>
+          <span>
+            1.5k
+          </span>
+        </div>
+        <p>
+          Nuestra deliciosa hamburguesa de carne de res X 2 preparada con los
+          mejores ingredientes y la sensacional salsa blanca para disfrutar en
+          cualquier momento del día.\r\n\r\nQué incluye: 150g de carne de res X
+          2, pan, tomate, lechuga y queso doble crema.
+        </p>
+      </div>
+    </section>
+
+    <div class="line-horizontal"></div>
+
+    <section>
+      <img
+        src="http://localhost:8000/media/burguer_ltZlWTt.jpg"
+        alt="Hamburguesa doble carne"
+      />
+      <div class="text-item">
+        <div class="title">
+          <h3>Hamburguesa doble carne</h3>
+          <span>
+            1.5k
+          </span>
+        </div>
+        <p>
+          Nuestra deliciosa hamburguesa de carne de res X 2 preparada con los
+          mejores ingredientes y la sensacional salsa blanca para disfrutar en
+          cualquier momento del día.\r\n\r\nQué incluye: 150g de carne de res X
+          2, pan, tomate, lechuga y queso doble crema.
+        </p>
+      </div>
+    </section> -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ListProduct",
+  data() {
+    return {
+      products: []
+    };
+  },
+  created() {
+    axios.get("http://localhost:8000/product/").then(result => {
+      console.log(result);
+      this.products = result.data;
+    });
+  }
+};
+</script>
+
+<style>
+.list {
+  display: flex;
+  flex-direction: column;
+}
+
+header {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  text-align: center;
+  background: #000;
+  color: white;
+}
+
+img {
+  height: 203px;
+}
+
+.title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text-item {
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+}
+
+img {
+  width: 100%;
+}
+
+span {
+  font-weight: bold;
+}
+
+.line-horizontal {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  background: #f0eded;
+  height: 1px;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+}
+</style>
